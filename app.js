fd55e276ec907e71e709bfb77d7a7d44b32157e6
P Aiswarya <91377431+aiswarya10102002@users.noreply.github.com>
2024-02-08 17:56:41 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,139 @@
+var form = document.getElementById("myForm"),
+    imgInput = document.querySelector(".img"),
+    file = document.getElementById("imgInput"),
+    userName = document.getElementById("name"),
+    age = document.getElementById("age"),
+    city = document.getElementById("city"),
+    
+    sDate = document.getElementById("sDate"),
+    submitBtn = document.querySelector(".submit"),
+    userInfo = document.getElementById("data"),
+    modal = document.getElementById("userForm"),
+    modalTitle = document.querySelector("#userForm .modal-title"),
+    newUserBtn = document.querySelector(".newUser")
+
+
+let getData = localStorage.getItem('userProfile') ? JSON.parse(localStorage.getItem('userProfile')) : []
+
+let isEdit = false, editId
+showInfo()
+
+newUserBtn.addEventListener('click', ()=> {
+    submitBtn.innerText = 'Submit',
+    modalTitle.innerText = "Save your info"
+    isEdit = false
+    imgInput.src = "doc.png"
+    form.reset()
+})
+
+
+file.onchange = function(){
+    if(file.files[0].size < 1000000){  // 1MB = 1000000
+        var fileReader = new FileReader();
+
+        fileReader.onload = function(e){
+            imgUrl = e.target.result
+            imgInput.src = imgUrl
+        }
+
+        fileReader.readAsDataURL(file.files[0])
+    }
+    else{
+        alert("This file is too large!")
+    }
+}
+
+
+function showInfo(){
+    document.querySelectorAll('.employeeDetails').forEach(info => info.remove())
+    getData.forEach((element, index) => {
+        let createElement = `<tr class="employeeDetails">
+            <td>${index+1}</td>
+            <td><img src="${element.picture}" alt="" width="50" height="50"></td>
+            <td>${element.employeeName}</td>
+            <td>${element.employeeAge}</td>
+            <td>${element.employeeCity}</td>
+            <td>${element.startDate}</td>
+
+
+            <td >
+                <button style="background-color:#B500FF;color:black"class="btn btn-success" onclick="readInfo('${element.picture}', '${element.employeeName}', '${element.employeeAge}', '${element.employeeCity}','${element.startDate}')" data-bs-toggle="modal" data-bs-target="#readData"><i class="bi bi-eye"></i></button>
+
+                <button style="background-color:#DDA0DD;color:black"class="btn btn-primary" onclick="editInfo(${index}, '${element.picture}', '${element.employeeName}', '${element.employeeAge}', '${element.employeeCity}', '${element.startDate}')" data-bs-toggle="modal" data-bs-target="#userForm"><i class="bi bi-pencil-square"></i></button>
+
+                <button style="background-color:#FFB6C1;color:black"class="btn btn-danger" onclick="deleteInfo(${index})"><i class="bi bi-trash"></i></button>
+                            
+            </td>
+        </tr>`
+
+        userInfo.innerHTML += createElement
+    })
+}
+showInfo()
+
+
+function readInfo(pic, name, age, city,sDate){
+    document.querySelector('.showImg').src = pic,
+    document.querySelector('#showName').value = name,
+    document.querySelector("#showAge").value = age,
+    document.querySelector("#showCity").value = city,
+    document.querySelector("#showsDate").value = sDate
+}
+
+
+function editInfo(index, pic, name, Age, City,Sdate){
+    isEdit = true
+    editId = index
+    imgInput.src = pic
+    userName.value = name
+    age.value = Age
+    city.value =City
+    sDate.value = Sdate
+
+    submitBtn.innerText = "Update"
+    modalTitle.innerText = "Update The Form"
+}
+
+
+function deleteInfo(index){
+    if(confirm("Are you sure want to delete?")){
+        getData.splice(index, 1)
+        localStorage.setItem("userProfile", JSON.stringify(getData))
+        showInfo()
+    }
+}
+
+
+form.addEventListener('submit', (e)=> {
+    e.preventDefault()
+
+    const information = {
+        picture: imgInput.src == undefined ? "doc.png" : imgInput.src,
+        employeeName: userName.value,
+        employeeAge: age.value,
+        employeeCity: city.value,
+        startDate: sDate.value
+    }
+
+    if(!isEdit){
+        getData.push(information)
+    }
+    else{
+        isEdit = false
+        getData[editId] = information
+    }
+
+    localStorage.setItem('userProfile', JSON.stringify(getData))
+
+    submitBtn.innerText = "Submit"
+    modalTitle.innerHTML = "Save your info"
+
+    showInfo()
+
+    form.reset()
+
+    imgInput.src = "doc.png"  
+
+    // modal.style.display = "none"
+    // document.querySelector(".modal-backdrop").remove()
+})
